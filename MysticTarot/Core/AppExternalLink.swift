9ed5446806332debffb4ecdb42884624b2248b24
Roman <romazaniosdev@gmail.com>
2026-05-07 20:00:40 +0300

    final

--- MysticTarot/Core/AppExternalLink.swift
+++ MysticTarot/Core/AppExternalLink.swift
@@ -13,9 +13,9 @@ enum AppExternalLink {
     var url: URL? {
         switch self {
         case .privacyPolicy:
-            return URL(string: "https://example.com/privacy-policy")
+            return URL(string: "https://www.termsfeed.com/live/fe71956d-b84b-445d-8671-64b8a6553c37")
         case .termsOfUse:
-            return URL(string: "https://example.com/terms")
+            return URL(string: "https://www.termsfeed.com/live/3958434e-792a-4872-b075-f3d813ef8a6f")
         }
     }
 }
